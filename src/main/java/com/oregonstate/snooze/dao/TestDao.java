a52127ae797045131d6856b9b6ac9884ff4b2dd2
lirance <lirance@users.noreply.github.com>
2018-09-27 16:02:57 -0700

    add some plugins

--- src/main/java/com/oregonstate/snooze/dao/TestDao.java
+++ src/main/java/com/oregonstate/snooze/dao/TestDao.java
@@ -4,7 +4,7 @@ package com.oregonstate.snooze.dao;
  * @author: Chendi Zhang
  * @date: 9/27/18
  * @description:
- */
+ **/
 
-public class testMapper {
+public interface TestDao {
 }
---
+++ src/main/java/com/oregonstate/snooze/model/User.java
@@ -0,0 +1,10 @@
+package com.oregonstate.snooze.model;
+
+/**
+ * @author: Chendi Zhang
+ * @date: 9/27/18
+ * @description:
+ **/
+
+public class User {
+}
