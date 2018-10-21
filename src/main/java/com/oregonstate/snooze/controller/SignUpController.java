c5d570907128001a2b242771f5d37a9d53d1f619
XintongWang <wangxint@oregonstate.edu>
2018-10-21 12:03:19 -0700

    username update

--- src/main/java/com/oregonstate/snooze/controller/SignUpController.java
+++ src/main/java/com/oregonstate/snooze/controller/SignUpController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
-
 @Controller
 @RequestMapping("/snooze")
 public class SignUpController {
