e4c27dc25229c249b8d1b2faba871080b2d956ff
lirance <lirance@users.noreply.github.com>
2018-11-05 02:54:05 -0800

    add session attributes

--- src/main/java/com/oregonstate/snooze/controller/LogInController.java
+++ src/main/java/com/oregonstate/snooze/controller/LogInController.java
@@ -4,11 +4,14 @@ import com.oregonstate.snooze.model.User;
 import com.oregonstate.snooze.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 
 @Controller
+@SessionAttributes({"email"})
 @RequestMapping("/snooze")
 public class LogInController {
 
@@ -17,9 +20,10 @@ public class LogInController {
 
     @RequestMapping(value = "/logIn")
     @ResponseBody
-    public boolean logIn(String inputPassword,String inputEmail) {
+    public boolean logIn(String inputPassword, String inputEmail, ModelMap map) {
         User user = userService.selectByEmail(inputEmail);
         if (user != null) {
+            map.addAttribute("email", user.geteMail());
             return (user.getPassword()).equals(Integer.toString(inputPassword.hashCode()));
         } else {
             return false;
