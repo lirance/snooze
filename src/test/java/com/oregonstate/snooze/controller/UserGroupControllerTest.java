13cd96803d1b560af110f9315e0b51000fd9a0d4
lirance <lirance@users.noreply.github.com>
2018-11-19 17:17:10 -0800

    unit test

---
+++ src/test/java/com/oregonstate/snooze/controller/UserGroupControllerTest.java
@@ -0,0 +1,26 @@
+package com.oregonstate.snooze.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserGroupControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void groupManager() {
+    }
+
+    @Test
+    public void groupGeneral() {
+    }
+}
